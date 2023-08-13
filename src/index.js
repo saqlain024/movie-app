5b91a28874e0882d568d8eee57b76c4d8716d9a1
Saklain Ahmad <saklainhmd@gmail.com>
2023-08-13 18:38:42 +0530

    MiddleWare done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
+//curried fxn
+//function logger(obj, next, action)
+//logger(obj)(next)(action)     this is how we call
+const logger = function ({dispatch, getState}) {
+  return function(next) {
+    return function(action) {
+      //middleware code
+      console.log('ACTION_TYPE = ' , action.type);
+      next(action);
+    }
+  }
+}
 
-const store = createStore(rootReducer);
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store', store);
 
 
