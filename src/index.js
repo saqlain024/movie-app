75ab8b2cdee00e343d6f9fcc8fe7adadf8844572
Saklain Ahmad <saklainhmd@gmail.com>
2023-08-13 11:18:21 +0530

    subscribe to the store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { createStore } from 'redux';
+import { createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit'
 
 import './index.css';
@@ -8,30 +8,30 @@ import App from './components/App';
 import movies from './reducers';
 
 
-// const store = createStore(movies);
-// console.log('store', store);
-// console.log('STATE', store.getState());
-const store = configureStore({
-  reducer: {
-    movies: movies,
-  }
-})
+const store = createStore(movies);
 console.log('store', store);
-console.log(' BEFORE STATE', store.getState());
+// console.log('STATE', store.getState());
+// const store = configureStore({
+//   reducer: {
+//     movies: movies,
+//   }
+// })
+// console.log('store', store);
+// console.log(' BEFORE STATE', store.getState());
 
-store.dispatch({
-  type: 'ADD_MOVIES',
-  movies: [{name: 'Superman'}]
-});
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{name: 'Superman'}]
+// });
 
-console.log(' AFTER STATE', store.getState());
+// console.log(' AFTER STATE', store.getState());
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
 
